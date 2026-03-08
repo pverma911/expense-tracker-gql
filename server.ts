a6b1641aa7b6d111073dd54aa8de32ac1fb14c1e
pverma911 <verma.pranshu689@gmail.com>
2026-03-08 12:52:14 +0530

    update(): prisma setup

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 // The ApolloServer constructor requires two parameters: your schema
 
 import { ApolloServer } from "@apollo/server";
-import { typeDefs } from "./schema";
-import { resolvers } from "./resolver";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./src/schema";
+import { resolvers } from "./src/resolver";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "./src/prisma/prisma/generated/client";
 
 // definition and your set of resolvers.
 
@@ -11,17 +13,20 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 
 (async () => {
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    typeDefs: typeDefs,
+    resolvers: resolvers,
   });
 
   // Passing an ApolloServer instance to the `startStandaloneServer` function:
   //  1. creates an Express app
   //  2. installs your ApolloServer instance as middleware
   //  3. prepares your app to handle incoming requests
+  const adapter = new PrismaBetterSqlite3({ url: "file:./src/prisma/dev.db" });
+  new PrismaClient({ adapter });
+
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
 
-  console.log(`🚀  Server ready at: ${url}`);
+  console.log(`🚀 Server ready at: ${url}`);
 })();
---
+++ src/prisma/migrations/20260308041704_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
